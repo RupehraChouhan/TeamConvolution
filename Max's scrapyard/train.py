f0fe48af51037ebde380ce5ada10c8fd757d97cb
Spencer <splant.10@gmail.com>
2017-11-30 15:02:19 -0700

    my batch norm progress

--- Max's scrapyard/train.py
+++ Max's scrapyard/train.py
@@ -22,8 +22,9 @@ def model(x, dropout, is_training):
     #output: (13, 13, 32)
     conv2_w = tf.get_variable(shape = [5,5,16,32], name = 'conv2_w', initializer=tf.contrib.layers.xavier_initializer_conv2d())
     conv2_b = tf.Variable(tf.zeros(32), name = 'conv2_b')
-    conv2 = tf.nn.conv2d(pool1, conv2_w, strides = [1, 1, 1, 1], padding = 'VALID')
-    h_conv2 = tf.nn.relu(conv2 + conv2_b)
+    conv2 = tf.nn.conv2d(pool1, conv2_w, strides = [1, 1, 1, 1], padding = 'VALID') + conv2_b
+    batch_norm2 = batch_norm(conv2, is_training)
+    h_conv2 = tf.nn.relu(batch_norm2)
     pool2 = tf.nn.max_pool(h_conv2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')
     # print(pool2)  
     
@@ -32,8 +33,9 @@ def model(x, dropout, is_training):
     #output: (4, 4, 64)
     conv3_w = tf.get_variable(shape = [5,5,32,64], name = 'conv3_w', initializer=tf.contrib.layers.xavier_initializer_conv2d())
     conv3_b = tf.Variable(tf.zeros(64), name = 'conv3_b')
-    conv3 = tf.nn.conv2d(pool2, conv3_w, strides = [1, 1, 1, 1], padding = 'VALID')
-    h_conv3 = tf.nn.relu(conv3 + conv3_b)
+    conv3 = tf.nn.conv2d(pool2, conv3_w, strides = [1, 1, 1, 1], padding = 'VALID') + conv3_b
+    batch_norm3 = batch_norm(conv3, is_training)
+    h_conv3 = tf.nn.relu(batch_norm3)
     pool3 = tf.nn.max_pool(h_conv3, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')
     # print(pool3)      
     
@@ -53,7 +55,9 @@ def model(x, dropout, is_training):
     #output: (64)
     fc2_w = tf.get_variable(shape = [256, 64], name = "fc2_w", initializer = tf.contrib.layers.xavier_initializer())
     fc2_b = tf.Variable(tf.zeros(64), name = 'fc2_b')
-    h_fc2 = tf.nn.relu(tf.matmul(h_fc, fc2_w) + fc2_b)
+    summed= tf.matmul(h_fc, fc2_w) + fc2_b
+    batch_normfc2 = batch_norm(summed, is_training)
+    h_fc2 = tf.nn.relu(batch_normfc2)
     # print(h_fc2)    
     
     
@@ -67,12 +71,10 @@ def model(x, dropout, is_training):
     s2_b = tf.Variable(tf.zeros(10), name = 's2_b')
     logit2 = tf.matmul(h_fc2, s2_w) + s2_b    
     
-
-    
     return logit1, logit2
 
 
-def train(EPOCHS=150, BATCH_SIZE=64, drop_out=0.5, lr=0.001):
+def train(EPOCHS=150, BATCH_SIZE=64, drop_out=0.1, lr=0.005):
     
     train_set = TextureImages('train', batch_size=BATCH_SIZE)
     valid_set = TextureImages('valid', shuffle=False)
@@ -84,9 +86,8 @@ def train(EPOCHS=150, BATCH_SIZE=64, drop_out=0.5, lr=0.001):
     print("label shape:", labels[0].shape)
        
     x = tf.placeholder(tf.float32, (None, 64, 64, 1))
-    y = tf.placeholder(tf.int32, (None,2))    
-    is_training = tf.placeholder(tf.bool, ())
-    
+    y = tf.placeholder(tf.int32, (None,2))
+    is_training = True
     logit1, logit2 = model(x, drop_out, is_training = is_training)
     
     ##reshape the flattened vector into a image
@@ -114,6 +115,8 @@ def train(EPOCHS=150, BATCH_SIZE=64, drop_out=0.5, lr=0.001):
     #prediction = tf.equal(tf.argmax(logit1, 1), tf.argmax(y, 1)) and tf.equal(tf.argmax(logit2, 1), tf.argmax(y, 2))
         
     #accuracy = tf.reduce_mean(tf.cast(prediction, tf.float32))
+    saver = tf.train.Saver()
+
     steps  = BATCH_SIZE * EPOCHS
     sess = tf.Session()
     with sess:
@@ -125,13 +128,33 @@ def train(EPOCHS=150, BATCH_SIZE=64, drop_out=0.5, lr=0.001):
                 print("step: ", i, "; Current loss: ", l)
                 acc = accuracy(l1,l2,batch_y)
                 print("Training accuracy: ", acc)
+                saver.save(sess, 'ckpt/teamConvolution', global_step=i)
 
-        # Need to do a validation here after all the training
-        
+def test(EPOCHS=150, BATCH_SIZE=64, drop_out=0.5, lr=0.001):
+
+    valid_set = TextureImages('valid', shuffle=False)
+    tf.reset_default_graph()
+       
+    x = tf.placeholder(tf.float32, (None, 64, 64, 1))
+    y = tf.placeholder(tf.int32, (None,2))
+    is_training = False
+    logit1, logit2 = model(x, drop_out, is_training = is_training)
+    
+    global_step = tf.Variable(0)
+    decaylr = tf.train.exponential_decay(lr, global_step, 5000, 0.096)
+
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = logit1, labels = y[:, 0])) + tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits = logit2, labels = y[:, 1]))
+    
+    optimizer = tf.train.AdamOptimizer(learning_rate = lr).minimize(loss, global_step = global_step)
+
+    valids1 = []
+    valids2 = []
+    validys = []
+
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint('ckpt'))
         validation_steps = math.ceil(len(valid_set.images) / BATCH_SIZE)
-        valids1 = []
-        valids2 = []
-        validys = []
         for i in range(validation_steps):
             valid_x, valid_y = valid_set.get_next_batch()
             valid1, valid2 = sess.run([logit1, logit2], feed_dict = {x:valid_x})
@@ -151,8 +174,8 @@ def train(EPOCHS=150, BATCH_SIZE=64, drop_out=0.5, lr=0.001):
             else:
                 validys = np.concatenate((validys, valid_y), axis=0)
 
-        acc = accuracy(valids1, valids2, validys)
-        print("Validation accuracy: ", acc)
+    acc = accuracy(valids1, valids2, validys)
+    print("Validation accuracy: ", acc)
 
     return acc
 
@@ -161,12 +184,12 @@ if __name__ == "__main__":
     grid_search = True
 
     if (not grid_search):
-        train()
+        test()
     else:
-        epochs = [150]
+        epochs = [150, 100]
         batch_sizes = [64, 32, 16]
-        dropout = [0.5, 0.8, 0.7, 0.6, 0.9, 0.4, 0.3, 0.2, 0.1]
-        learning_rates = [0.001, 0.002, 0.003, 0.004, 0.005, 0.0009, 0.0008, 0.0007, 0.0006, 0.0005, 0.01]
+        dropout = [0.5, 0.8, 0.7, 0.6, 0.4, 0.3, 0.2, 0.1]
+        learning_rates = [0.001, 0.002, 0.003, 0.004, 0.005, 0.0009, 0.0008, 0.0007, 0.0006, 0.0005]
 
         print("Running Training with grid search " + str(len(epochs) * len(batch_sizes) * len(dropout) * len(learning_rates)) + " times.")
 
@@ -182,7 +205,8 @@ if __name__ == "__main__":
                         my_file = open("hypers.csv", "a+")
                         my_file.write(str(e) + ", " + str(b) + ", " + str(d) + ", " + str(lr) + ", ")
                         my_file.close()
-                        acc = train(EPOCHS=e, BATCH_SIZE=b, drop_out=d, lr=lr)
+                        train(EPOCHS=e, BATCH_SIZE=b, drop_out=d, lr=lr)
+                        acc = test(EPOCHS=e, BATCH_SIZE=b, drop_out=d, lr=lr)
                         my_file = open("hypers.csv", "a+")
                         my_file.write(str(acc) + "\n")
                         my_file.close()
--- Max's scrapyard/utility.py
+++ Max's scrapyard/utility.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 BATCH_SIZE = 64
-def batch_norm(input, is_training, momentum=0.9, epsilon=1e-5, in_place_update=True, name="batch_norm"):
+def batch_norm(input, is_training, momentum=0.9, epsilon=1e-5, in_place_update=True):
   # Example batch_norm usage:
   # bn0 = batch_norm(conv1, is_training=is_training, name='bn0')
   if in_place_update:
@@ -11,16 +11,14 @@ def batch_norm(input, is_training, momentum=0.9, epsilon=1e-5, in_place_update=T
                                         scale=True,
                                         epsilon=epsilon,
                                         updates_collections=None,
-                                        is_training=is_training,
-                                        scope=name)
+                                        is_training=is_training)
   else:
     return tf.contrib.layers.batch_norm(input,
                                         decay=momentum,
                                         center=True,
                                         scale=True,
                                         epsilon=epsilon,
-                                        is_training=is_training,
-                                        scope=name)
+                                        is_training=is_training)
   
   
   
